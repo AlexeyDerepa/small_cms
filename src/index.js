d28d34f7b91c040b7b44295611026632b8bc90da
Alexey Derepa <graf_dav2@ukr.net>
2018-01-17 17:29:55 +0200

    deleted some comments

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 
 import store from './store';
-import MainPage from './containers/mainPage';
 import Start from './containers';
 ReactDOM.render(
     <Provider store={store}>
